68153dc0971342067997c65183d19c3f514b033b
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-01-15 18:13:46 +0300

    Update notes.store.js

--- js/stores/notes.store.js
+++ js/stores/notes.store.js
@@ -1,1 +1,49 @@
+// Create Note
+function addNote(note) {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction(["notes"], "readwrite");
+    const store = transaction.objectStore("notes");
+    note.id = Date.now().toString();
+    note.dateCreated = new Date().toISOString();
+    store.add(note);
 
+    transaction.oncomplete = () => resolve(note);
+    transaction.onerror = () => reject("Add note failed");
+  });
+}
+
+// Read All Notes
+function getAllNotes() {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction(["notes"], "readonly");
+    const store = transaction.objectStore("notes");
+    const request = store.getAll();
+
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject("Read notes failed");
+  });
+}
+
+// Update Note
+function updateNote(note) {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction(["notes"], "readwrite");
+    const store = transaction.objectStore("notes");
+    store.put(note);
+
+    transaction.oncomplete = () => resolve(note);
+    transaction.onerror = () => reject("Update note failed");
+  });
+}
+
+// Delete Note
+function deleteNote(id) {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction(["notes"], "readwrite");
+    const store = transaction.objectStore("notes");
+    store.delete(id);
+
+    transaction.oncomplete = () => resolve();
+    transaction.onerror = () => reject("Delete note failed");
+  });
+}
