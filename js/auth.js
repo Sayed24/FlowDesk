df4362035251acc69a5a3bcbfe15ea1752d7c033
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-01-29 20:31:15 -0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,40 @@
+import { initDB, getStore } from "./db/indexeddb.js";
+
+await initDB();
+
+export async function register(email, password) {
+  const hash = await crypto.subtle.digest(
+    "SHA-256",
+    new TextEncoder().encode(password)
+  );
+
+  const store = getStore("users", "readwrite");
+  store.add({
+    email,
+    password: Array.from(new Uint8Array(hash))
+  });
+}
+
+export async function login(email, password) {
+  const store = getStore("users");
+  store.get(email).onsuccess = async (e) => {
+    const user = e.target.result;
+    if (!user) return alert("User not found");
+
+    const hash = await crypto.subtle.digest(
+      "SHA-256",
+      new TextEncoder().encode(password)
+    );
+
+    if (
+      JSON.stringify(user.password) ===
+      JSON.stringify(Array.from(new Uint8Array(hash)))
+    ) {
+      localStorage.setItem("session", email);
+      location.href = "index.html";
+    } else {
+      alert("Wrong password");
+    }
+  };
+}
 
