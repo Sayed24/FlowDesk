5fd724c60be65d83c25a5b1aa04721dc9e4234cb
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-02-02 23:42:43 -0800

    Update shortcuts.js

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -1,1 +1,6 @@
+document.addEventListener("keydown", e => {
+  if (e.key === "n") location.href = "notes.html";
+  if (e.key === "t") location.href = "tasks.html";
+  if (e.key === "c") location.href = "calendar.html";
+});
 
